7dae5ac7ecdf0ad2684b555cfaf70af4f104db6c
AndrewCMoore <47224173+AndrewCMoore@users.noreply.github.com>
2019-09-21 00:42:34 -0400

    feat: Updating UDP Sender Java File

--- UDPSender.java
+++ UDPSender.java
@@ -1,12 +1,15 @@
+import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
 public class UDPSender {
 
+	private final static int PACKETSIZE = 100 ;
+	
 	public static void main(String[] args) 
    {
-	      // Check the arguments
-	      if( args.length != 2 )
+		// Check the arguments
+	      if( args.length != 3 )
 	      {
 	         System.out.println( "usage: java UDPSender host port" ) ;
 	         return ;
@@ -17,21 +20,38 @@ public class UDPSender {
 	         // Convert the arguments first, to ensure that they are valid
 	         InetAddress host = InetAddress.getByName( args[0] ) ;
 	         int port         = Integer.parseInt( args[1] ) ;
+	         int n 			  = Integer.parseInt( args[2] ) ;
 	         socket = new DatagramSocket() ;
      
 	         Scanner in;
 	         in = new Scanner (System.in);
 	         String message = null;
+
 	         while (true)
 	         {
-	        		 System.out.println("Enter text to be sent, ENTER to quit ");
-	        		 message = in.nextLine();
-	        		 if (message.length()==0) break;
+	        	 for(int i = 0; i < n; i++) {
+	        		 message = "Message" + i;
 	        		 byte [] data = message.getBytes() ;
 	        		 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
 	        		 socket.send( packet ) ;
+	        		 
+	        		 System.out.println( "Receiving on port " + port ) ;
+		 		     DatagramPacket packet2 = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE );
+		 		     
+		 		     // Sleeps the thread to make sure that the viewer can see the response before the sending
+		 		     try {
+		 		    	 Thread.sleep(1000);
+		 		    	 socket.receive( packet2 ) ;
+		 		     } catch(IOException e) {
+		 		    	 e.printStackTrace();
+		 		    	 System.exit(1);
+		 		     }
+		 		     
+		 	         String message2 = new String(packet.getData()).trim();
+			         System.out.println("ACK: " + message2 ) ;
+	        	 }
+	 
 	         } 
-	         System.out.println ("Closing down");
 	      }
 	      catch( Exception e )
 	      {
@@ -44,4 +64,3 @@ public class UDPSender {
       }
    }
 }
-
