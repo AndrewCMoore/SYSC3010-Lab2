3c5da02bb6e142d6f4b6b5756396720c08a9c325
AndrewCMoore <47224173+AndrewCMoore@users.noreply.github.com>
2019-09-20 23:56:51 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -9,10 +9,13 @@ n = int(sys.argv[3])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
+
+#creates and binds a server address for the returning message to the sender
 server_address2 = (host, 4)
 s.bind(server_address2)
 
 while 1:
+	#while in range of the loop, do this call n amount of times
 	for x in range(n):
 	  print ("Enter data to transmit: ENTER to quit")
 	  data = sys.stdin.readline().strip()
@@ -22,10 +25,12 @@ while 1:
 	  s.sendto(data.encode('utf-8'), server_address)
 
 	  print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+	  # wait to receive on port 4   
 	  buf, address = s.recvfrom(4)
 	  if not len(buf):
 	    	  break
-	  print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+	  string = str(buf, 'utf-8')
+	  print ("ACK:", string)
 	
 s.shutdown(1)
 
